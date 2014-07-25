80d7419e4cbb232078b5ee5107bc386ada530faf
James Tomlinson <tomo.bbe@gmail.com>
2014-07-25 23:54:08 +0100

    Add first FSA test with corresponding additions to cvodes

--- src/pySundials/cvodes.pyx
+++ src/pySundials/cvodes.pyx
@@ -24,6 +24,132 @@ include 'cvodes_properties.pxi'
 
 cdef class Cvodes(BaseCvodes):
     
-    def __init__(self, *args, **kwds):        
-        self._nrtfn = kwds.pop('nrtfn', 0)
+    def initSens(self, int num_sensitivities, method, yS0, rhs_type='all'):
+        cdef int ism
         
+        if method == 'simultaneous':
+            ism = cvode.CV_SIMULTANEOUS
+        elif method == 'staggered':
+            ism = cvode.CV_STAGGERED
+        elif method == 'staggered1':
+            ism = cvode.CV_STAGGERED1
+        else:
+            raise CVodeError()
+
+        # Convert list of N_Vectors into pointers           
+        cdef int n = len(yS0)
+        cdef int i
+        cdef sun.N_Vector *yS0_ptrs = <sun.N_Vector *>malloc(n*cython.sizeof(<sun.N_Vector*>NULL))
+        
+        for i in range(n):
+            yS0_ptrs[i] = (<N_Vector>yS0[i])._v
+        
+        if rhs_type == 'all':
+            ret = cvode.CVodeSensInit(self._cv, num_sensitivities, ism,
+                                  _CvSensRhsFn, yS0_ptrs)
+        else:
+            ret = cvode.CVodeSensInit1(self._cv, num_sensitivities, ism,
+                                  _CvSensRhsFn1, yS0_ptrs)
+        if ret != 0:
+            raise CVodeError()
+
+    def setSensTolerances(self, reltol=None, abstol=None):
+        
+        if reltol is None and abstol is None:
+            ret = cvode.CVodeSensEEtolerances(self._cv)
+        else:
+            raise NotImplementedError()
+        if ret != 0:
+            raise CVodeError()
+            
+    def sensErrCon(self, sun.booleantype errconS):
+        
+        ret = cvode.CVodeSetSensErrCon(self._cv, errconS)
+        if ret != 0:
+            raise CVodeError()
+
+    def sensParams(self, sun.realtype[::1] p=None, sun.realtype[::1] pbar=None, int[::1] plist=None):
+        
+        cdef sun.realtype* p_ = NULL
+        cdef sun.realtype* pbar_ = NULL
+        cdef int* plist_ = NULL
+        
+        if not p is None: p_ = &p[0]
+        if not pbar is None: pbar_ = &pbar[0]
+        if not plist is None: plist_ = &plist[0]
+            
+        
+        
+        ret = cvode.CVodeSetSensParams(self._cv, p_, pbar_, plist_)
+        if ret != 0:
+            raise CVodeError()
+            
+            
+    def SolveSens(self, ysout):
+        cdef sun.realtype tret     
+        
+        # Convert list of N_Vectors into pointers           
+        cdef int n = len(ysout)
+        cdef int i
+        cdef sun.N_Vector *yS_ptrs = <sun.N_Vector *>malloc(n*cython.sizeof(<sun.N_Vector*>NULL))
+        
+        for i in range(n):
+            yS_ptrs[i] = (<N_Vector>ysout[i])._v        
+        
+        cdef int flag = cvode.CVodeGetSens(self._cv, &tret, yS_ptrs)
+        return cvode.CVodeGetReturnFlagName(flag), tret
+        
+        
+    def SensRhsFn(self, Ns, t, y, ydot, yS, ySdot, tmp1, tmp2):
+        raise NotImplementedError()
+        
+    def SensRhsFn1(self, Ns, t, y, ydot, iS, yS, ySdot, tmp1, tmp2):
+        raise NotImplementedError()        
+
+cdef int _CvSensRhsFn(int Ns, sun.realtype t,
+			   sun.N_Vector y, sun.N_Vector ydot,
+			   sun.N_Vector *yS, sun.N_Vector *ySdot,
+			   void *user_data,
+			   sun.N_Vector tmp1, sun.N_Vector tmp2):
+          
+    cdef object obj
+    cdef int i
+    
+    obj = <object>user_data
+    
+    pyy = <object>y.content
+    pyydot = <object>ydot.content
+    pytmp1 = <object>tmp1.content
+    pytmp2 = <object>tmp2.content
+    
+    pyys = []
+    pyysdot = []
+    
+    for i in range(Ns):
+        pyys.append( <object>yS[i].content )
+        pyysdot.append( <object>ySdot[i].content )
+
+    return obj.SensRhsFn(Ns, t, pyy, pyydot, pyys, pyysdot, pytmp1, pytmp2)
+    
+
+           
+cdef int _CvSensRhsFn1(int Ns, sun.realtype t,
+			   sun.N_Vector y, sun.N_Vector ydot,
+			   int iS, sun.N_Vector yS, sun.N_Vector ySdot,
+			   void *user_data,
+			   sun.N_Vector tmp1, sun.N_Vector tmp2):
+          
+    cdef object obj
+    cdef int i
+    
+    obj = <object>user_data
+    
+    pyy = <object>y.content
+    pyydot = <object>ydot.content
+    pytmp1 = <object>tmp1.content
+    pytmp2 = <object>tmp2.content
+    
+    pyys = <object>yS.content
+    pyysdot = <object>ySdot.content
+
+    return obj.SensRhsFn1(Ns, t, pyy, pyydot, iS, pyys, pyysdot, pytmp1, pytmp2)           
--- src/tests/examples/cvodes/testCvsRobertsFSA.py
+++ src/tests/examples/cvodes/testCvsRobertsFSA.py
@@ -33,11 +33,12 @@
 
 from pySundials.cvodes import Cvodes
 from pySundials.sundials import NvectorNdarrayFloat64
-
+import numpy as np
 
 # Problem Constants 
 
 NEQ   = 3                # number of equations  
+NS = 3
 Y1    = 1.0             # initial y components 
 Y2    = 0.0
 Y3    = 0.0
@@ -53,6 +54,11 @@ NOUT  = 12               # number of output times
 
 class Roberts(Cvodes):
     
+    def __init__(self, p, **kwds):
+        Cvodes.__init__(self, **kwds)
+        
+        self.p = p
+    
     def RhsFn(self, t, y, ydot):
         """
          f routine. Compute function f(t,y).        
@@ -60,9 +66,10 @@ class Roberts(Cvodes):
         """
         
         y1 = y.data[0]; y2 = y.data[1]; y3 = y.data[2]
+        p1,p2,p3 = self.p
         
-        yd1 = ydot.data[0] = -0.04*y1 + 1.0e4*y2*y3
-        yd3 = ydot.data[2] = 3.0e7*y2*y2
+        yd1 = ydot.data[0] = -p1*y1 + p2*y2*y3
+        yd3 = ydot.data[2] = p3*y2*y2
         ydot.data[1] = -yd1 - yd3
         
         return 0
@@ -95,8 +102,45 @@ class Roberts(Cvodes):
         J[2,1] = 6.0e7*y2
 
         return 0        
+        
+    def SensRhsFn1(self, Ns, t, y, ydot, iS, yS, ySdot, tmp1, tmp2):
+        
+        y1 = y.data[0]; y2 = y.data[1]; y3 = y.data[2]
+        s1 = yS.data[0]; s2 = yS.data[1]; s3 = yS.data[2]
+        p1,p2,p3 = self.p
+
+        sd1 = -p1*s1 + p2*y3*s2 + p2*y2*s3;
+        sd3 = 2*p3*y2*s2;
+        sd2 = -sd1-sd3;
+        
+        if iS == 0:
+            sd1 += -y1
+            sd2 +=  y1
+        elif iS == 1:    
+            sd1 +=  y2*y3
+            sd2 += -y2*y3
+        elif iS == 2:
+            sd2 += -y2*y2
+            sd3 +=  y2*y2
+    
+        ySdot.data[0] = sd1
+        ySdot.data[1] = sd2
+        ySdot.data[2] = sd3
+        
+        return 0
+
 
  
+    def ErrWeightFn(self, y, w,):         
+        atol = [ATOL1, ATOL2, ATOL3]
+     
+        for i in range(3):
+           yy = y.data[i]
+           ww = RTOL * abs(yy) + atol[i]
+           if ww <= 0.0: return -1
+           w.data[i] = 1.0/ww
+         
+        return 0
 
 
  
@@ -105,6 +149,14 @@ class Roberts(Cvodes):
 
 def PrintOutput( t, y1, y2, y3):
     print "At t = %0.4e      y =%14.6e  %14.6e  %14.6e" % ( t, y1, y2, y3)
+
+
+def PrintOutputS(uS):
+    
+    for i in range(len(uS)):
+        sdata = uS[i].data
+        print "                  Sensitivity %d  %12.4e %12.4e %12.4e" %(i+1, sdata[0], sdata[1], sdata[2]);
+
 
 
 def PrintRootInfo( root_f1, root_f2):
@@ -136,7 +188,7 @@ if __name__ == '__main__':
     # Initialize y 
     y.data[:] = [Y1,Y2,Y3]
     
-
+    p = np.array([0.04, 1.0e4, 3.0e7])
     # Set the scalar relative tolerance 
     reltol = RTOL;
     # Set the vector absolute tolerance 
@@ -146,7 +198,7 @@ if __name__ == '__main__':
     # Call CVodeCreate to create the solver memory and specify the 
     # Backward Differentiation Formula and the use of a Newton iteration 
     # Call CVodeRootInit to specify the root function g with 2 components   
-    cvode_mem = Roberts(multistep='bdf', iteration='newton', nrtfn=2)
+    cvode_mem = Roberts(p, multistep='bdf', iteration='newton', nrtfn=2)
   
     # Call CVodeInit to initialize the integrator memory and specify the
     # user's right hand side function in y'=f(t,y), the inital time T0, and
@@ -156,12 +208,21 @@ if __name__ == '__main__':
 
     # Call CVodeSVtolerances to specify the scalar relative tolerance
     # and vector absolute tolerances 
-    cvode_mem.setTolerances(reltol, abstol)
+    cvode_mem.setTolerances(None, None)
 
     # Call CVDense to specify the CVDENSE dense linear solver   
     # Set the Jacobian routine to Jac (user-supplied) 
     cvode_mem.setupDenseLinearSolver( NEQ, user_jac=True )
   
+
+    yS = [NvectorNdarrayFloat64(NEQ) for i in range(NS)]
+    cvode_mem.initSens(NS, 'simultaneous', yS, 'one')
+    
+    cvode_mem.setSensTolerances()
+    cvode_mem.sensErrCon(True)
+    
+    
+    cvode_mem.sensParams(None, p, None)
 
     # In loop, call CVode, print results, and test for error.
     # Break out of loop when NOUT preset output times have been reached.  
@@ -170,19 +231,27 @@ if __name__ == '__main__':
 
     iout = 0;  tout = T1;
     while True:
-        flag, t = cvode_mem.Solve(tout, y)
-        
+        flag, t = cvode_mem.Solve(tout, y)        
         PrintOutput(t, y.data[0], y.data[1], y.data[2] )
         
         if flag == 'CV_ROOT_RETURN':
             rootsfound = cvode_mem.rootInfo()
             PrintRootInfo(rootsfound[0], rootsfound[1])
             
-        if flag == 'CV_SUCCESS':
+        elif flag == 'CV_SUCCESS':
             iout += 1
-            tout *= TMULT
+        else:
+            break
+
+        flag, t = cvode_mem.SolveSens( yS )
+
+        PrintOutputS( yS )
+        
+        print "-----------------------------------------"
+        print "------------------------------"        
         
         if iout == NOUT: break
+        tout *= TMULT
         
     # Print some final statistics 
     PrintFinalStats(cvode_mem)        
